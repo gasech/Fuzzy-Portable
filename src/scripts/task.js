4aa30de5d6ad0fc256f59463c1f44b0eea80ed62
Gabriel <gsechaus@gmail.com>
2022-03-07 03:26:36 -0300

    :sparkles: Tasks now saves in Local Storage

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -1,35 +1,71 @@
-let idCounter = -1;
+let idCounter = 0;
 
 let tasks = [];
 
-function createTask() {
-    let inputName = document.getElementById("input-name").value;
-    let inputDesc = document.getElementById("input-desc").value;
+function loadTasks() {
+    if (localStorage.getItem("taskList")) {
+        let localTasks = JSON.parse(localStorage.getItem("taskList"));
 
-    if (inputName.length > 18) return alert("Please use 18 characters or less in task name.");
-    if (inputDesc.length > 40) return alert("Please use 40 characters or less in task description.");
-    if (inputName == "" || inputName == " ") return alert("Please do not leave the name area empty.");
+        for (let task of localTasks) {
+            createTask(task);
+            idCounter++;
+        }
+    }
+}
 
-    idCounter++;
-    tasks.push({ id: idCounter, name: inputName, desc: inputDesc, status: "to do" });
+loadTasks();
 
-    let currentTask = tasks[tasks.length - 1];
+function createTask(task) {
+    let inputName, inputDesc;
+    let statusColor = "#F50C55";
+    let currentTask;
 
-    document.getElementById("input-name").value = "";
-    document.getElementById("input-desc").value = "";
+    if (task) {
+        tasks.push(task);
+        currentTask = task;
+    } else {
+        inputName = document.getElementById("input-name").value;
+        inputDesc = document.getElementById("input-desc").value;
 
-    let ul = document.getElementById("task-test");
-    let li = document.createElement("li");
-    li.setAttribute('id', `task-${currentTask.id}`);
-    li.innerHTML = `<div id="generalstatus">
+        if (inputName.length > 18) return alert("Please use 18 characters or less in task name.");
+        if (inputDesc.length > 40) return alert("Please use 40 characters or less in task description.");
+        if (inputName == "" || inputName == " ") return alert("Please do not leave the name area empty.");
+        document.getElementById("input-name").value = "";
+        document.getElementById("input-desc").value = "";
+        document.getElementById("input-name").select();
+
+        idCounter++;
+        tasks.push({ id: idCounter, name: inputName, desc: inputDesc, status: "to do" });
+        currentTask = tasks[tasks.length - 1];
+        localStorage.setItem("taskList", JSON.stringify(tasks));
+    }
+
+    if (currentTask.status != "deleted") {
+        let ul = document.getElementById("task-test");
+        let li = document.createElement("li");
+        li.setAttribute('id', `task-${currentTask.id}`);
+        li.innerHTML = `<div id="generalstatus">
 	                    <div class="statusball" id="task-status-${currentTask.id}" onclick="toggleTask(${currentTask.id})"></div>
-	                    <div class="statusname" id="task-name-${currentTask.id}">${currentTask.name} -</div>
+	                    <div class="statusname" id="task-name-${currentTask.id}">${currentTask.name} </div>
 	                    <div class="statusdesc" id="task-desc-${currentTask.id}">${currentTask.desc}</div>
                         <div class="status-delete" id="task-delete-${currentTask.id}"><button class="btn" onclick="deleteTask(${currentTask.id})"><i class="fa fa-trash"></i></button></div>
                     </div>`;
-    ul.appendChild(li);
+        ul.appendChild(li);
 
-    document.getElementById("input-name").select();
+        switch (currentTask.status) {
+            case "to do":
+                statusColor = "#F50C55";
+                break;
+            case "doing":
+                statusColor = "#FFE03D";
+                break;
+            case "done":
+                statusColor = "#0CF51A";
+                break;
+        }
+
+        document.getElementById(`task-status-${currentTask.id}`).style.background = statusColor;
+    }
 }
 
 function deleteTask(id) {
@@ -39,6 +75,7 @@ function deleteTask(id) {
         }
     }
 
+    localStorage.setItem("taskList", JSON.stringify(tasks));
     document.getElementById(`task-${id}`).style.display = "none";
 }
 
@@ -47,15 +84,15 @@ function toggleTask(id) {
         if (id == task.id) {
             switch (task.status) {
                 case "to do":
-                    task.status = updatedStatus = "doing";
+                    task.status = "doing";
                     document.getElementById(`task-status-${task.id}`).style.background = "#FFE03D";
                     break;
                 case "doing":
-                    task.status = updatedStatus = "done";
+                    task.status = "done";
                     document.getElementById(`task-status-${task.id}`).style.background = "#0CF51A";
                     break;
                 case "done":
-                    task.status = updatedStatus = "to do";
+                    task.status = "to do";
                     document.getElementById(`task-status-${task.id}`).style.background = "#F50C55";
                     break;
                 default:
@@ -63,4 +100,6 @@ function toggleTask(id) {
             }
         }
     }
+
+    localStorage.setItem("taskList", JSON.stringify(tasks));
 }
